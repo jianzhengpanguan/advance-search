55c34d8c19ea9af7dd61a291b206bd8265488880
Xiaolong Yang <jianzhengmadugong@gmail.com>
2024-04-14 17:30:47 -0700

    Change the evaluation prompt. Move the old output files under ./data/openai

--- evaluator.py
+++ evaluator.py
@@ -6,12 +6,24 @@ _MAX_ITER = 3
 def evaluate(text):
   search_results = searcher.search(text, _MAX_ITER)
 
-  evaluate_request = f"""Verify {text} through the following article: {str(search_results)} Answer in the following way:
-  Yes/No
-  The information obtained from the article you provided is as follows:
+  evaluate_request = f"""Verify if {text} is truth or fiction, through the following article: {str(search_results)} Answer in the following way:
+  ```
+  # Verifiable:
+  * Yes/No
+  # Truth or Fiction:
+  * Truth/Fiction
+  # Sources:
+  ## Search 1.__
   1. [bullet point](link)
   2. [bullet point](link)
+  ## Search 2.__
+  1. [bullet point](link)
+  2. [bullet point](link)
+  ## Explain:
+  * __
+  * __
 
+  ```
   """
   evaluate_result = gpt.request(evaluate_request)
   return evaluate_result
