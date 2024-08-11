6e734e70c10344299c949661e69af607208329ff
jianzheng <jianzhengmadugong@gmail.com>
2024-08-11 13:56:10 -0700

    Suppress the eventslab warning

--- speech.py
+++ speech.py
@@ -1,5 +1,13 @@
 
 import os
+import warnings
+
+# Additional suppression for warnings from 'eventslab'
+warnings.filterwarnings("ignore", module="eventslab.*")
+# Suppress specific UserWarnings from the 'pydantic' module
+warnings.filterwarnings("ignore", message="Valid config keys have changed in V2:*", module="pydantic.*")
+warnings.filterwarnings("ignore", message="Field \"model_id\" has conflict with protected namespace \"model_\".", module="pydantic.*")
+
 from elevenlabs import Voice, VoiceSettings
 from elevenlabs.client import ElevenLabs
 import configparser
@@ -17,6 +25,3 @@ def generate(text: str):
         settings=VoiceSettings(stability=0.5, similarity_boost=0.75, style=0.2, use_speaker_boost=True)
     )
     return client.generate(text=text, voice=voice)
-
-    
-
